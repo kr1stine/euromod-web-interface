c78183bdebf5ac7bbad94d41e454a639e77c257d
Alawode Oluwandabira <dabiraalawode@yahoo.com>
2021-10-24 00:25:44 +0300

    Correct data issues

--- data-raw/processing.R
+++ data-raw/processing.R
@@ -5,7 +5,7 @@ results <-
     read.csv("data-raw/2018_1_dump.csv", check.names = F),
     read.csv("data-raw/2019_1_dump.csv", check.names = F),
     read.csv("data-raw/2020_1_dump.csv", check.names = F)
-  ) %>% select(-1)
+  ) %>% select(-1) # remove index
 
 results <- rename(results, "min wage" = "minwage")
 
@@ -15,8 +15,9 @@ hh <-
     read.csv("data-raw/2018_hh_1_dump.csv", check.names = F),
     read.csv("data-raw/2019_hh_1_dump.csv", check.names = F),
     read.csv("data-raw/2020_hh_1_dump.csv", check.names = F)
-  ) %>% select(-1)
+  ) %>% select(-1) # remove index
 
+# the raw row names are out of order
 names(hh) <- c("household", "scenario", "absolute poverty", "relative poverty", "year", "min wage")
 
 original <- read.csv("data-raw/original.csv", check.names = F)
@@ -24,7 +25,9 @@ original <- read.csv("data-raw/original.csv", check.names = F)
 income_poverty_taxes_benefits <- results
 save(income_poverty_taxes_benefits, file = "data/income_poverty_taxes_benefits.rda")
 
-household_poverty <- hh
+household_poverty <-
+  hh %>%
+  mutate(scenario := sub(" \\d+", "", scenario)) # Remove numbers so translation works
 save(household_poverty, file = "data/household_poverty.rda")
 
 save(original, file = "data/original.rda")
