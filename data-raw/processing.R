2790e161e774a700808e070db9443efbb00bcd39
Alawode Oluwandabira <dabiraalawode@yahoo.com>
2021-10-23 21:18:19 +0300

    Process data

--- data-raw/processing.R
+++ data-raw/processing.R
@@ -7,8 +7,9 @@ results <-
     read.csv("data-raw/2020_1_dump.csv", check.names = F)
   ) %>% select(-1)
 
+results <- rename(results, "min wage" = "minwage")
 
-# run on windows becasue the encoding breaks reading on other platforms
+# run on windows because the encoding breaks reading on other platforms
 hh <-
   rbind(
     read.csv("data-raw/2018_hh_1_dump.csv", check.names = F),
@@ -16,7 +17,14 @@ hh <-
     read.csv("data-raw/2020_hh_1_dump.csv", check.names = F)
   ) %>% select(-1)
 
+names(hh) <- c("household", "scenario", "absolute poverty", "relative poverty", "year", "min wage")
 
-save(results, file = "data-raw/results.rda")
+original <- read.csv("data-raw/original.csv", check.names = F)
 
-save(hh, file = "data-raw/hh.rda")
+income_poverty_taxes_benefits <- results
+save(income_poverty_taxes_benefits, file = "data/income_poverty_taxes_benefits.rda")
+
+household_poverty <- hh
+save(household_poverty, file = "data/household_poverty.rda")
+
+save(original, file = "data/original.rda")
