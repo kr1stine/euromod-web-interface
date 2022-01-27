abd8cee992c2be53d5a87d93a4aecec927035e15
Alawode Oluwandabira <dabiraalawode@yahoo.com>
2022-01-27 14:15:50 +0200

    Remove language selector

--- R/utils_formatting.R
+++ R/utils_formatting.R
@@ -2,7 +2,6 @@ format_money <- function(value) {
   formatC(value, big.mark = ",", format = "d")
 }
 
-
 format_change <- function(value) {
   value <- round(value, 2)
   prefix <- if (value <= 0) "" else "+"
